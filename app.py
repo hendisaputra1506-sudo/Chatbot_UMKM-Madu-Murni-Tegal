8decbfbe22a1c117b07287d32f5e846628b372d6
Hendi <hendisaputra1506@gmail.com>
2026-01-27 04:49:38 +0700

    rework

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, jsonify
+from core import get_bot_reply
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.get_json() or {}
+    user_message = data.get("message", "")
+    reply = get_bot_reply(user_message)
+    return jsonify({"reply": reply})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
